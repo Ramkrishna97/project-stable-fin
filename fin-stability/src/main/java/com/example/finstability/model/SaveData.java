74c06bc96b450215cfcc0a8707fe0858fe79ff32
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-01-25 13:59:35 +0530

    Added actual logic ,working fine

--- fin-stability/src/main/java/com/example/finstability/model/SaveData.java
+++ fin-stability/src/main/java/com/example/finstability/model/SaveData.java
@@ -1,16 +1,10 @@
 package com.example.finstability.model;
 
 //import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-import tools.jackson.databind.annotation.JsonPOJOBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//@Entity
-//@JsonPOJOBuilder
 public class SaveData {
 
 //    @Id
