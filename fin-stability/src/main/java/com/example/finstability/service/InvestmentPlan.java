6a07b024d4c9a239f8ac67cceeaf877017e02c0f
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-02-02 02:39:06 +0530

    Making changes to actual logic, for better code.

--- fin-stability/src/main/java/com/example/finstability/service/InvestmentPlan.java
+++ fin-stability/src/main/java/com/example/finstability/service/InvestmentPlan.java
@@ -8,24 +8,24 @@ import org.springframework.stereotype.Service;
 public class InvestmentPlan {
 //    @Autowired
     static SaveData data;
-    public static long beforeRetirementPlan(double balance , double monthlyInvest,
-                                            double investIncrement, double yearlyInterestRate ,
-                                            double yearsUntilRetirement)
-    {
-        double monthlyInterestRate=yearlyInterestRate/1200;
-        double interest=0.0;
-        long finalSaving=0L;
-        balance+=monthlyInvest;
-
-        for(int i=0;i<yearsUntilRetirement;i++){
-            if(i%2==0){monthlyInvest+=investIncrement;}
-            interest += balance* monthlyInterestRate*12;
-            balance+=monthlyInvest;
-        }
-        System.out.println("balance after invest : "+balance+"\n interest after invest: "+interest);
-        finalSaving+=balance+interest;
-//        System.out.println("Final saving received: "+finalSaving);
-//        data.setBalance_After_Investment(finalSaving);
-        return finalSaving;
-    }
+//    public static long beforeRetirementPlan(double balance , double monthlyInvest,
+//                                            double investIncrement, double yearlyInterestRate ,
+//                                            double yearsUntilRetirement)
+//    {
+//        double monthlyInterestRate=yearlyInterestRate/1200;
+//        double interest=0.0;
+//        long finalSaving=0L;
+//        balance+=monthlyInvest;
+//
+//        for(int i=0;i<yearsUntilRetirement;i++){
+//            if(i%2==0){monthlyInvest+=investIncrement;}
+//            interest += balance* monthlyInterestRate*12;
+//            balance+=monthlyInvest;
+//        }
+//        System.out.println("balance after invest : "+balance+"\n interest after invest: "+interest);
+//        finalSaving+=balance+interest;
+////        System.out.println("Final saving received: "+finalSaving);
+////        data.setBalance_After_Investment(finalSaving);
+//        return finalSaving;
+//    }
 }
