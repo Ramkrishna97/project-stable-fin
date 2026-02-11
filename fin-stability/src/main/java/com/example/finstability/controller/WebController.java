1e619acd04c9ee4c04ba742946aefe7dcd04aef9
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-02-12 02:51:11 +0530

    Working code -Making changes to actual logic, for better code.

--- fin-stability/src/main/java/com/example/finstability/controller/WebController.java
+++ fin-stability/src/main/java/com/example/finstability/controller/WebController.java
@@ -2,13 +2,16 @@ package com.example.finstability.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
-
+    @PostMapping("/save")
     public void takeData(){
         //POST method that takes data from web , save
     }
+    @GetMapping("/getData")
     public void showData(){
         // GET method to show list of data
     }
