15e31bd4cc0b659cc0ad2df1501974d123ccfdee
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-02-17 12:27:22 +0530

    Working code -Making changes to actual logic, for better code.

--- fin-stability/src/main/java/com/example/finstability/FinStabilityApplication.java
+++ fin-stability/src/main/java/com/example/finstability/FinStabilityApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinStabilityApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinStabilityApplication.class, args);
-    }
 
+    }
 }
