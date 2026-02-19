0ea26eb0a39e65036296409f67f0dabc45a97c6a
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-02-19 11:11:58 +0530

    Working code -Making changes to actual logic, for better code.

--- fin-stability/src/main/java/com/example/finstability/FinStabilityApplication.java
+++ fin-stability/src/main/java/com/example/finstability/FinStabilityApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinStabilityApplication {    public static void main(String[] args) {
         SpringApplication.run(FinStabilityApplication.class, args);
-        System.out.println("System Started .  .  .");
+        System.out.println("System Started . .  .");
     }
 }
