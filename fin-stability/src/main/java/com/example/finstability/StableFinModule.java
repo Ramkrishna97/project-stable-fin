de67e5083a3b228d4e9ae01fc89067b15f8f9097
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-07-04 10:15:12 +0530

    Single original application

---
+++ fin-stability/src/main/java/com/example/finstability/StableFinModule.java
@@ -0,0 +1,136 @@
+package com.example.finstability;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StableFinModule {
+    public static void main(String[] args) {
+        System.out.println("Financial Stability Module has Awaken.\n");
+
+        /*
+        i>.________before retirement_________
+        invest monthly / yearly ;
+        apply interest monthly / yearly ;
+        count balance till retirement.
+        increment in invest occur after sometime.
+
+        ii>._______at retirement__________
+        balance fixed;
+        monthly / yearly withdraw started;
+        yearly/monthly interest adds ;      if interest is more that withdraw, re-invest it.
+
+        iii>._______give suggestion________
+        if, saved balance doesn't meet expectation , suggest what to do-
+         - need to increate investment ?
+         - what target investment will give desired result.
+        */
+
+        //    double r=6.0; // interest rate per year , on investment
+        //    double currentYear=2026, retirementYear=2056;
+        //    double time = 10
+        // //    retirementYear - currentYear
+        //    ;
+
+
+        //     long totalSaving=beforeRetirementPlan(0, 46000,5000, r, time);
+        //     double balance=totalSaving;
+        //     retirementPlan(balance, 6  , 25000,
+        //          6 , time, 25);
+        New.execute();
+
+
+    }
+    // public static long beforeRetirementPlan(double balance , double monthlyInvest,double investIncrement, double yearlyInterestRate ,double yearsUntilRetirement){
+    //     double monthlyInterestRate=yearlyInterestRate/1200;
+    //     double interest=0.0;
+    //     long finalSaving=0L;
+    //     balance+=monthlyInvest;
+    //     for(int i=0;i<yearsUntilRetirement;i++){
+    //         if(i%2==0){monthlyInvest+=investIncrement;}
+    //     interest += balance* monthlyInterestRate*12;
+    //     balance+=monthlyInvest;
+    //     }
+    //     System.out.println("balance after invest : "+balance+"\ninterest after invest: "+interest);
+    //     finalSaving+=balance+interest;
+    //     System.out.println("Total saving before retirement : "+finalSaving);
+    //     return finalSaving;
+    // }
+
+    public static long beforeRetirementPlan(
+            double balance,
+            double monthlyInvest,
+            double investIncrement,
+            double yearlyInterestRate,
+            double yearsUntilRetirement) {
+
+        for (int year = 1; year <= yearsUntilRetirement; year++) {
+            balance += monthlyInvest * 12;
+            balance *= (1 + yearlyInterestRate / 100.0);
+            if (year % 2 == 0) {
+                monthlyInvest += investIncrement;
+            }
+        }
+        long finalSaving = (long) balance;
+        System.out.println("Total saving before retirement : " + finalSaving);
+        return finalSaving;
+    }
+
+    public static void retirementPlan( double balance ,double interestRate,
+                                       double currentMonthlyExpense ,double inflation , double yearsUntilRetirement,
+                                       double nextTargetYear)
+    {
+        /*
+        -calculates required monthly expense by implying inflation to current monthly expense.
+         no monthly investment, as no income
+        -check monthly interest on available lump-sum balance ,
+        -if interest is more than monthly expense , save extra as investment
+        -otherwise, return that available amount not sustainable: calculate how long it will last
+            - for this , withdraw monthly expense from interest , withdraw extra from balance.
+        */
+        List<String> futureData=new ArrayList<>();
+        futureData.add("\nTotal Lump-sum balance saved till retirement: "+balance);
+
+        double futureMonthlyExpense = (currentMonthlyExpense*12* Math.pow((1+inflation/100),yearsUntilRetirement))/12;
+        futureData.add("\nFuture monthly expense, to maintain as per present expenses:"+futureMonthlyExpense);
+
+        double monthly_Interest_On_Total_Saving=balance*interestRate/1200;
+        futureData.add("\nMonthly interest received from saving as income:"+monthly_Interest_On_Total_Saving);
+        System.out.println("\nfuture monthly expense required calculated as : "+futureMonthlyExpense+" \nmonthly_Interest_On_Total_Saving :"+monthly_Interest_On_Total_Saving);
+
+        if(monthly_Interest_On_Total_Saving>futureMonthlyExpense){
+            for(int i=0;i<nextTargetYear;i++){
+                balance+=monthly_Interest_On_Total_Saving-futureMonthlyExpense;
+                futureData.add("\nAvailable balance and interest is enough to last :"+nextTargetYear+" years");
+                break;
+            }
+        }
+        else{
+            System.out.println("Available amount not sustainable .");
+            int yearsLeft=0;
+            while(balance>0){
+                balance-=futureMonthlyExpense-monthly_Interest_On_Total_Saving;
+                yearsLeft++;
+            }
+            futureData.add("\nAmount will last only for : "+yearsLeft+"  years");
+        }
+        futureData.add("\n");
+        System.out.println(futureData);
+    }
+
+
+}
+
+
+class New{
+    public static void execute() {
+        int[] num={1,2,4,3,5,7,8,90,0,00,12,35,14,47,560,013};
+        String[] nam={"harish","amish","hari","Aman","Kesab","Jack","daniel","jojo"};
+
+        Arrays.stream(num).filter(n -> n > 20).forEach(System.out::println);
+        // System.out.println(out);
+
+    }
+}
+
+
